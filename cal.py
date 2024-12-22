6e8324107c97bc119850f4d9c396ac76f35d101d
khurram676 <khurramshabir475@gmail.com>
2024-12-21 21:44:32 -0800

    function in py

---
+++ cal.py
@@ -0,0 +1,18 @@
+def sum(a,b): 
+    c=a+b
+    return c
+def sub(a,b):
+    c=a-b
+    return c
+def div(a,b):
+    c=a/b
+    return c
+def mul(a,b):
+    c=a*b
+    return c
+num1=int(input("enter a the number 1: "))
+num2=int(input("enter a the number 1: "))
+print("the result is: ",sum(num1,num2))
+print("the result is: ",sub(num1,num2))
+print("the result is: ",div(num1,num2))
+print("the result is: ",mul(num1,num2))
---
+++ func.py
@@ -0,0 +1,5 @@
+def display(name,age):
+      print("hello",name,"your age is",age)
+name=(input("ENTER YOUR NAME: "))
+age=(int(input("ENTER YOUR AGE: ")))
+display(name,age)
